c8bfd1e188cb0bb4e755a81b5f1e17c469f19698
mauro <mrb0305@gmail.com>
2025-04-21 19:04:35 -0300

    styled componente

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import { Groups } from '@screens/Groups';
+import Theme from '@theme/index';
+import { ThemeProvider } from 'styled-components';
 
 export default function App() {
   return (
+    <ThemeProvider theme={Theme}>
     <Groups/>
+    </ThemeProvider>
   );
 }
 
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -4,10 +4,7 @@ export const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
-    background-color: #000;
+    background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+    padding: 24px;
 `;
-
-export const Title = styled.Text`
-    font-size: 32px;
-    color: #fff;    
-    `;            
+          
